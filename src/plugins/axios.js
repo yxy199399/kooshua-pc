022e3afa3dba1e6ad88458fec1246fdf03d42811
yxy <2828122019@qq.com>
2019-08-01 14:25:49 +0800

    pre-commit

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -18,42 +18,42 @@ let config = {
   timeout: 2500, // Timeout
 }
 
-const _axios = axios.create (config)
+const _axios = axios.create(config)
 
 //loading
 let loadingInstance
 
-_axios.interceptors.request.use (
+_axios.interceptors.request.use(
   config => {
     // eslint-disable-next-line no-console
-    console.log (process.env.VUE_APP_BASEURL)
-    loadingInstance = Loading.service ({
+    console.log(process.env.VUE_APP_BASEURL, 111)
+    loadingInstance = Loading.service({
       text: '加载中...',
       background: 'rgba(255, 255, 255, 0.2)',
     })
 
-    if (localStorage.getItem ('token')) {
-      config.headers['Api-Access-Token'] = localStorage.getItem ('token')
+    if (localStorage.getItem('token')) {
+      config.headers['Api-Access-Token'] = localStorage.getItem('token')
     }
     // Do something before request is sent
     return config
   },
   error => {
     // Do something with request error
-    return Promise.reject (error)
+    return Promise.reject(error)
   }
 )
 
 // Add a response interceptor
-_axios.interceptors.response.use (
+_axios.interceptors.response.use(
   res => {
-    if (loadingInstance) loadingInstance.close ()
+    if (loadingInstance) loadingInstance.close()
     //301 重新登录
     // Do something with response data
     return res
   },
   error => {
-    return Promise.reject (error)
+    return Promise.reject(error)
   }
 )
 
@@ -67,31 +67,31 @@ let initApiConfig = key => {
 
 /***
  ** 暴露请求方法
-***/
+ ***/
 export default {
   get: (key, params) => {
-    let url = initApiConfig (key)
-    return _axios.get (url, {params: params})
+    let url = initApiConfig(key)
+    return _axios.get(url, {params: params})
   },
 
   post: (key, data) => {
-    let url = initApiConfig (key, data)
-    return axios.post (url, data)
+    let url = initApiConfig(key, data)
+    return axios.post(url, data)
   },
 
   put: (key, data) => {
-    let url = initApiConfig (key, data)
-    return axios.put (url, data)
+    let url = initApiConfig(key, data)
+    return axios.put(url, data)
   },
 
   delete: (key, params) => {
-    let url = initApiConfig (key, params)
-    return axios.delete (url, {params: params})
+    let url = initApiConfig(key, params)
+    return axios.delete(url, {params: params})
   },
 
   download: (key, params) => {
-    let url = initApiConfig (key, params)
-    return axios ({
+    let url = initApiConfig(key, params)
+    return axios({
       url: url,
       params: params,
       method: 'GET',
